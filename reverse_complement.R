b95481b99f2578147d53f2545c867c50a2537360
Gideon-Samuel <gideonsamueledith@gmail.com>
2025-09-08 22:17:03 +0530

    added comments!

--- reverse_complement.R
+++ reverse_complement.R
@@ -1,9 +1,14 @@
+#Using R library (Biostrings) for handling strings.
 library(Biostrings)
 
+#Asking for DNA sequence as input.
 x <- readline("Give DNA sequence: ")
 
+#DNAString() wraps into a Biostrings object.
 y <- DNAString(x)
 
+#Reverse Complementing.
 z <- reverseComplement(y)
 
+#Printing results.
 cat("Reverse Complement :", as.character(z), "\n")
